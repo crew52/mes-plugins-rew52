8f4f10037432c88be7e756ffe01b710f5e872d30
crew52 <dungnc150602@gmail.com>
2025-08-13 15:33:35 +0700

    feat: add validation of phone and hiredate

--- src/main/java/com/qcadoo/mes/rew52/validators/EmployeeValidators.java
+++ src/main/java/com/qcadoo/mes/rew52/validators/EmployeeValidators.java
@@ -5,12 +5,14 @@ import com.qcadoo.model.api.Entity;
 import com.qcadoo.model.api.FieldDefinition;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
 import java.util.Date;
 
 
 @Service
 public class EmployeeValidators {
     public static final String ERROR_AGE = "rew52.employee.dateOfBirth.invalidAge";
+    public static final String ERROR_HIRE_DATE = "rew52.employee.hireDate.invalidRange";
 
     public boolean validateAgeBetween18And35(final DataDefinition dataDefinition,
                                              final FieldDefinition fieldDefinition,
@@ -21,6 +23,33 @@ public class EmployeeValidators {
 
         if (!DateRangeUtils.isAgeBetween(dateOfBirth, 18, 35)) {
             entity.addError(fieldDefinition, ERROR_AGE);
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validateHireDateAfter18AndBeforeFuture1Month(final DataDefinition dataDefinition,
+                                                                final FieldDefinition fieldDefinition,
+                                                                final Entity entity,
+                                                                final Object oldValue,
+                                                                final Object newValue) {
+        Date hireDate = (Date) newValue;
+        Date dateOfBirth = (Date) entity.getField("dateOfBirth");
+
+        if (hireDate == null || dateOfBirth == null) {
+            entity.addError(fieldDefinition, ERROR_HIRE_DATE);
+            return false;
+        }
+
+        Calendar minAllowedDate = Calendar.getInstance();
+        minAllowedDate.setTime(dateOfBirth);
+        minAllowedDate.add(Calendar.YEAR, 18);
+
+        Calendar maxAllowedDate = Calendar.getInstance();
+        maxAllowedDate.add(Calendar.MONTH, 1); 
+        boolean valid = !(hireDate.before(minAllowedDate.getTime()) || hireDate.after(maxAllowedDate.getTime()));
+        if (!valid) {
+            entity.addError(fieldDefinition, ERROR_HIRE_DATE);
             return false;
         }
         return true;
