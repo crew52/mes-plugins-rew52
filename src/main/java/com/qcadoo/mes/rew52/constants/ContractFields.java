77f51d9702994adef32b8426394a5470b7de5ae8
crew52 <dungnc150602@gmail.com>
2025-08-18 14:52:04 +0700

    feat: create ContractFields

---
+++ src/main/java/com/qcadoo/mes/rew52/constants/ContractFields.java
@@ -0,0 +1,20 @@
+package com.qcadoo.mes.rew52.constants;
+
+public class ContractFields {
+    private ContractFields() {
+
+    }
+    // Contract Fields
+    public static final String NUMBER = "number";
+    public static final String CONTRACT_TYPE = "contractType";
+    public static final String START_DATE = "startDate";
+    public static final String END_DATE = "endDate";
+    public static final String BASIC_SALARY = "basicSalary";
+    public static final String STATUS = "status";
+    public static final String SIGNED_DATE = "signedDate";
+    public static final String ATTACHMENT = "attachment";
+    public static final String NOTES = "notes";
+
+    // Relationships
+    public static final String EMPLOYEE = "employee";
+}
