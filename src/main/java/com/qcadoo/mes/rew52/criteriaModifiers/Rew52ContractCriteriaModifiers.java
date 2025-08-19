b85113df57f779e6bf337d399d0c4ccd56c81689
crew52 <dungnc150602@gmail.com>
2025-08-19 10:09:34 +0700

    feat: show employee active = true

---
+++ src/main/java/com/qcadoo/mes/rew52/criteriaModifiers/Rew52ContractCriteriaModifiers.java
@@ -0,0 +1,18 @@
+package com.qcadoo.mes.rew52.criteriaModifiers;
+
+import com.qcadoo.mes.rew52.constants.EmployeeFields;
+import com.qcadoo.model.api.search.SearchCriteriaBuilder;
+import com.qcadoo.model.api.search.SearchRestrictions;
+import com.qcadoo.view.api.components.lookup.FilterValueHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Rew52ContractCriteriaModifiers {
+//    public void filterActiveEmployees(final SearchCriteriaBuilder scb, final FilterValueHolder filterValueHolder) {
+//        scb.add(SearchRestrictions.eq(EmployeeFields.ACTIVE, true));
+//    }
+
+    public void filterActiveEmployees(final SearchCriteriaBuilder scb) {
+        scb.add(SearchRestrictions.eq(EmployeeFields.ACTIVE, true));
+    }
+}
--- src/main/java/com/qcadoo/mes/rew52/criteriaModifiers/Rew52EmployeeCriteriaModifiers.java
+++ src/main/java/com/qcadoo/mes/rew52/criteriaModifiers/Rew52EmployeeCriteriaModifiers.java
@@ -1,13 +1,36 @@
 package com.qcadoo.mes.rew52.criteriaModifiers;
 
+import com.qcadoo.mes.rew52.constants.DepartmentFields;
 import com.qcadoo.mes.rew52.constants.EmployeeFields;
+import com.qcadoo.mes.rew52.constants.PositionFields;
 import com.qcadoo.model.api.search.SearchCriteriaBuilder;
 import com.qcadoo.model.api.search.SearchRestrictions;
+import com.qcadoo.view.api.components.lookup.FilterValueHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Rew52EmployeeCriteriaModifiers {
+    private static final String DEPARTMENT_CODE_KEY = "code";
+    private static final String POSITION_KEY = "notName";
     public void showEmployeeWithoutDepartment(final SearchCriteriaBuilder scb) {
         scb.add(SearchRestrictions.isNull(EmployeeFields.DEPARTMENT));
+    }
+
+    public void selectPosition(final SearchCriteriaBuilder scb, final FilterValueHolder filterValueHolder) {
+        if (filterValueHolder.has(POSITION_KEY)) {
+            String value = filterValueHolder.getString(POSITION_KEY);
+            if (value != null && !value.isEmpty()) {
+                scb.add(SearchRestrictions.ne(PositionFields.NAME, value));
+            }
+        }
+    }
+
+    public void selectDepartment(final SearchCriteriaBuilder scb, final FilterValueHolder filterValueHolder) {
+        if (filterValueHolder.has(DEPARTMENT_CODE_KEY)) {
+            String value = filterValueHolder.getString(DEPARTMENT_CODE_KEY);
+            if (value != null && !value.isEmpty()) {
+                scb.add(SearchRestrictions.eq(DepartmentFields.CODE, value));
+            }
+        }
     }
 }
