b84647cc32ecd1f2e29ae17ac93d8fcbd87eb045
crew52 <dungnc150602@gmail.com>
2025-08-20 10:23:06 +0700

    feat: show form import

---
+++ src/main/java/com/qcadoo/mes/rew52/listeners/EmployeesListListeners.java
@@ -0,0 +1,13 @@
+package com.qcadoo.mes.rew52.listeners;
+
+import com.qcadoo.view.api.ComponentState;
+import com.qcadoo.view.api.ViewDefinitionState;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeesListListeners {
+    public void openEmployeesImportPage(final ViewDefinitionState view, final ComponentState state, final String[] args) {
+        StringBuilder url = new StringBuilder("../page/rew52/employeesImport.html");
+        view.openModal(url.toString());
+    }
+}
