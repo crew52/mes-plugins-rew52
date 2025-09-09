a5a7cabb96735a8bb1aeb352ad4e878747b19c9c
crew52 <dungnc150602@gmail.com>
2025-09-09 11:36:08 +0700

    feat: show buttuon

--- src/main/java/com/qcadoo/mes/rew52/services/GanttWorkingShiftsService.java
+++ src/main/java/com/qcadoo/mes/rew52/services/GanttWorkingShiftsService.java
@@ -10,7 +10,6 @@ public class GanttWorkingShiftsService {
 
     public void showGanttWorkingShiftCalendar(final ViewDefinitionState viewDefinitionState, final ComponentState triggerState,
                                        final String[] args) {
-        System.out.println("showGanttWorkingShiftCalendar");
-        viewDefinitionState.openModal("../page/ganttForShifts/ganttForShifts.html");
+        viewDefinitionState.openModal("../page/rew52/ganttForWorkingShifts.html");
     }
 }
