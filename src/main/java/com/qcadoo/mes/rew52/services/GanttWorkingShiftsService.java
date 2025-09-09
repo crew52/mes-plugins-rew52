5cb8c902c0d3adc0ce4fee985fa0dd6a99717ff9
crew52 <dungnc150602@gmail.com>
2025-09-09 09:26:36 +0700

    feat: show button calendar

---
+++ src/main/java/com/qcadoo/mes/rew52/services/GanttWorkingShiftsService.java
@@ -0,0 +1,15 @@
+package com.qcadoo.mes.rew52.services;
+
+import org.springframework.stereotype.Service;
+
+import com.qcadoo.view.api.ComponentState;
+import com.qcadoo.view.api.ViewDefinitionState;
+
+@Service
+public class GanttWorkingShiftsService {
+
+    public void showGanttWorkingShiftCalendar(final ViewDefinitionState viewDefinitionState, final ComponentState triggerState,
+                                       final String[] args) {
+        viewDefinitionState.openModal("../page/ganttForShifts/ganttForShifts.html");
+    }
+}
