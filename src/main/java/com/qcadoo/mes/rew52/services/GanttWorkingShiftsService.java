63d98b43510c5717f654fa8deed7457454fda153
crew52 <dungnc150602@gmail.com>
2025-09-09 09:52:07 +0700

    feat: add listener

--- src/main/java/com/qcadoo/mes/rew52/services/GanttWorkingShiftsService.java
+++ src/main/java/com/qcadoo/mes/rew52/services/GanttWorkingShiftsService.java
@@ -10,6 +10,7 @@ public class GanttWorkingShiftsService {
 
     public void showGanttWorkingShiftCalendar(final ViewDefinitionState viewDefinitionState, final ComponentState triggerState,
                                        final String[] args) {
+        System.out.println("showGanttWorkingShiftCalendar");
         viewDefinitionState.openModal("../page/ganttForShifts/ganttForShifts.html");
     }
 }
